4a904f7f1aabd16524983db8ce65a8081cf1bd01
pig <qkqkzzzz@gmail.con>
2016-02-27 00:13:19 +0800

    add practice

---
+++ images/sprites.js
@@ -0,0 +1,15 @@
+{
+"frames": {
+	"capguy-walk.png": {
+		"frame": {"x":0, "y":0, "w":1454, "h":317},
+		"spriteSourceSize": {"x":15,"y":3,"w":1472,"h":325},
+		"sourceSize": {"w":1472,"h":325}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 2048, "h": 512},
+	"scale": "1"
+}
+}
